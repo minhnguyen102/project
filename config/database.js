81caff2e8f3cf020b55d112c83d944e4f86a21ea
CodeMnh <minhkhac1002@gmail.com>
2025-02-10 23:34:30 +0700

    chỉnh sửa trang chi tiết sản phẩm

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/project");
+        await mongoose.connect("mongodb://127.0.0.1:27017/product-management");
         console.log("Connect success");
     } catch (error){
         console.log("Connect Error");
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,8 +1,16 @@
 const Product = require("../../models/product.model")
 module.exports.index = async (req, res) =>  {
-    const products = await Product.find({});
-    console.log(products)
+    const products = await Product.find({
+        status : "active"
+    });
+    
+    const newProducts = products.map(item => {
+        item.newPrice = (item.price - item.price * item.discountPercentage / 100).toFixed(0);
+        return item;
+    })
+
     res.render("client/pages/products/index",{
-        pageTitle : "Trang sản phẩm"
+        pageTitle : "Trang sản phẩm",
+        products : newProducts
     })
 }
