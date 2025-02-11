4d18ab0d763e3aba4224ba7a39553f00399b4138
CodeMnh <minhkhac1002@gmail.com>
2025-02-11 09:09:09 +0700

    Làm giao diện trang danh sách sản phẩm - admin

--- routes/admin/index.router.js
+++ routes/admin/index.router.js
@@ -1,10 +1,12 @@
 const system = require("../../config/system")
 const dashboardRouter = require("./dashboard.router")
+const productsRouter = require("./products.router")
 
 module.exports = (app) =>{
     const PATH_ADMIN = system.prefixAdmin;
     // app.use("admin" + "/dashboard", dashboardRouter) dùng biến lưu trữ pathAdmin, phòng khi chỉnh sửa thì chỉ cần chỉnh 1 chỗ 
     app.use(PATH_ADMIN + "/dashboard", dashboardRouter)
+    app.use(PATH_ADMIN + "/products", productsRouter)
 }
 
-
+ 
