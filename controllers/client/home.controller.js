4f694cd6becc2de7db537510433e39ab80706fd2
CodeMnh <minhkhac1002@gmail.com>
2025-03-11 21:15:05 +0700

    Hiển thị sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,5 @@
 const Product = require("../../models/product.model")
-const productHelper = require("../../helpers/product")
+const productsHelper = require("../../helpers/product")
 
 // [GET] /home
 module.exports.index = async (req, res) => {
@@ -9,14 +9,22 @@ module.exports.index = async (req, res) => {
         featured : "1",
         status : "active"
     }
-
     const productsFeatured = await Product.find(find).limit(6).select("-createBy -updateBy");
-    const newProductsFeatured = productHelper.priceNewProduct(productsFeatured);
+    const newProductsFeatured = productsHelper.priceNewProduct(productsFeatured);
+
+    const productsNew = await Product.find({
+        deleted : false,
+        status : "active"
+    }).sort({createdAt : "desc"}).limit(6).select("-createBy -updateBy")
+    const newProductsNew = productsHelper.priceNewProduct(productsNew);
+
     res.render("client/pages/home/index",{
         pageTitle : "Trang chủ",
-        productsFeatured : newProductsFeatured
+        productsFeatured : newProductsFeatured,
+        newProductsNew : newProductsNew
     })
 
     // api
     // res.json(newProductsFeatured); // lay ra san pham noi bat 
+    // res.json(newProductsNew); // lay ra san pham moi
 }
