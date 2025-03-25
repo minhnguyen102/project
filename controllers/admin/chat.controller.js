530058674b1b4c2a38fb5fcb6b756c2ebbb1fb9e
CodeMnh <minhkhac1002@gmail.com>
2025-03-25 09:13:39 +0700

    Tính năng chat cơ bản

--- controllers/admin/chat.controller.js
+++ controllers/admin/chat.controller.js
@@ -1,10 +1,40 @@
+const Chat = require("../../models/chat.model")
+const Account = require("../../models/accounts.model")
+
 // [GET] /admin/dashboard
 module.exports.index = async (req, res) => {
-    _io.on("connection", (socket) => {
-        console.log('a user connected', socket.id)
+
+    const user_id = res.locals.user.id;
+    
+    //SOCKET (once : tránh tạo kết nối mới nhiều lần sau khi load lại trang web)
+    _io.once("connection", (socket) => {
+        // console.log('a user connected', socket.id)
+        socket.on("CLIENT_SEND_MESSAGE", async (content) =>{
+            const chat = new Chat({
+                user_id : user_id,
+                content : content
+            });
+
+            await chat.save();
+        })
     })
 
+    const chats = await Chat.find({
+        deleted : false
+    })
+    
+    for (const chat of chats) {
+        const inforUser = await Account.findOne({
+            _id : chat.user_id
+        }).select("fullname")
+        
+        chat.inforUser = inforUser;
+        
+    }
+    
+    //END SOCKET
     res.render("admin/page/chat/index",{
         pageTitle : "Trang Chat",
+        chats : chats
     })
 }
--- public/admin/js/chat.js
+++ public/admin/js/chat.js
@@ -1,1 +1,11 @@
-const socket = io();
+// CLIENT_SEND_MESSAGE
+const formSendMessage = document.querySelector(".chat .inner-form");
+formSendMessage.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const content = e.target.elements.content.value;
+    if(content){
+        socket.emit("CLIENT_SEND_MESSAGE", content);
+        e.target.elements.content.value = "";
+    }
+})
+// END CLIENT_SEND_MESSAGE
